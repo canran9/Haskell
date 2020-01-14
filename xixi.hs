28d1f1d2c03b1f4d146d8b3c24a21a9d1b1d420c
canran9 <58532953+canran9@users.noreply.github.com>
2020-01-14 21:35:20 +0100

    Delete xixi.hs

--- xixi.hs
+++
@@ -1,9 +0,0 @@
-data Nat = Zero | Succ Nat
-initVal :: Nat -> Integer
-initVal Zero = 0
-initVal (Succ x) = (initVal x)+1
-
- 
-add Zero y = y
-add (Succ x) y = Succ (add x y)
-	           add x (Succ y)
