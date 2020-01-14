8c21294cde7a22a7a444435aa1524a9e85c3c3e3
canran9 <58532953+canran9@users.noreply.github.com>
2020-01-14 21:34:10 +0100

    Delete ex37.hs

--- ex37.hs
+++
@@ -1,7 +0,0 @@
-map _ [] = []
-map f (x : xs ) = (f x ):( map f xs )
-
-map ::  (a ->b) -> [a] ->[b]
-
-flip :: (a ->b ->c) -> (b ->a ->c)
-flip           f                 x     y    =    x      f y x 
