150a134c0199ab7fe44a4bbc8af8ac8c34cb8e97
canran9 <58532953+canran9@users.noreply.github.com>
2020-01-14 21:34:24 +0100

    Delete ex42.hs

--- ex42.hs
+++
@@ -1,8 +0,0 @@
-diviseurs :: Interger -> [Interger]
-//diviseurs n = [a | a<- [ . . n ], rem n a == 0 ]
-diviseurs n = filter ((==0).(  mod n)) [1 . . n]
-
-
-premiers :: Interger -> [Interger]
-premiers n = filter ((==2).length.diviseurs) [1 . . n]
-
